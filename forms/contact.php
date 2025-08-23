4f3363c8436aea6b68ea6ae0c920abbb700208a3
sebby-wekesa <sebbywakis@gmail.com>
2025-08-23 17:02:43 +0300

    modifile my rusume page

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-  $receiving_email_address = 'sebbywakis@gmailS.com';
+  $receiving_email_address = 'sebbywakis@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
